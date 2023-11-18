95a190e340a5bdced607a7f5c58f86ea5f94355d
talantasan <talant.py@gmail.com>
2023-11-18 01:13:56 +0000

    web development continues

--- web1/web.py
+++ web1/web.py
@@ -4,15 +4,19 @@ import requests
 
 app = Flask(__name__)
 
-employee_all = requests.get("http://20.55.62.182:8080/employee")
-
 @app.route('/')
 def home():
     return render_template('home.html', utc_dt=datetime.utcnow())
 
 @app.route('/employee')
 def employee():
-    return render_template('employee.html')
+    employee_all = requests.get("http://20.55.62.182:8080/employee")
+    employee_all = employee_all.json()
+    return render_template('employee.html', employee=employee_all)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
             
 if __name__ == '__main__':
     app.run(port=5001, host='0.0.0.0', debug=True, threaded=True)
